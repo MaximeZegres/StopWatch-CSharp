8f1aed06964591f02498f0802b9eb32978b24277
maxime.zegres <maxime.zegres@gmail.com>
2018-11-13 20:50:39 +0100

    Initial Commit - StopWatch Exercise

---
+++ StopWatch/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace StopWatch
+{
+    class Program
+    {
+        static void Main(string [] args)
+        {
+            StopWatch stopWatch = new StopWatch();
+
+            for (var i = 0; i < 1; i++)
+            {
+                stopWatch.Start(DateTime.Now);
+
+                    for (var j = 0; j <= 500; j++)
+                    {
+                        Thread.Sleep(1);
+                    }
+
+                stopWatch.Stop(DateTime.Now);
+
+                Console.WriteLine("Duration " + stopWatch.GetInterval().ToString());
+                Console.ReadKey();
+            }
+
+        }
+
+
+        
+
+        
+
+
+
+    }
+}
---
+++ StopWatch/StopWatch.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StopWatch
+{
+    class StopWatch
+    {
+        private DateTime _startTime { get; set; }
+        private DateTime _stopTime { get; set; }
+
+        private bool _running = false;
+
+
+        public void Start(DateTime start)
+        {
+            if (!_running)
+            {
+                _startTime = start;
+                _running = true;
+            }
+            else
+            {
+                throw new InvalidOperationException("Stopwatch is already running");
+            }        
+        }
+
+
+        public void Stop(DateTime stop)
+        {
+            if (_running)
+            {
+                _stopTime = stop;
+                _running = false;
+            }
+        }
+
+        public TimeSpan GetInterval()
+        {
+            var durationTime = _stopTime - _startTime;
+            return durationTime;
+        }
+        
+
+    }
+}
